61192b04ea0fb7f1e742279e995ab01598e0091a
Darshan429 <darshan99015@gmail.com>
2026-06-19 08:35:04 +0530

    Configure ChromeOptions and modify URL access
    Added ChromeOptions for headless browsing and updated the URL.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
@@ -7,17 +8,17 @@ import org.openqa.selenium.By;
 public class App {
     public static void main(String[] args) {
 
+        ChromeOptions options = new ChromeOptions();
+        options.addArgument("--headless=new");
+        options.addArgument("--no-sandbox");
+        options.addargument("--disable-dev-shm-usage");
+        options.addArgument("--disable-gpu");
+
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
-
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-
-        System.out.println("Login Successful");
-
+        driver.get("https://wwww.saucedemo.com");
+        driver.System.out.println(driver.getTitle());
         driver.quit();
+
     }
 }
